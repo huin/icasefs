43ffad53571ff23acffb892100335b38c4fe7643
John Beisley <greatred@gmail.com>
2012-05-13 16:19:16 +0100

    Revamped tests.

--- test.sh
+++ test.sh
@@ -3,62 +3,66 @@
 MNT="scratch/mount"
 ROOT="scratch/root"
 
-function setup() {
-    mkdir -p "$MNT" || exit 1
+function FATAL() {
+    echo "FATAL: $*" >&2
+    exit 1
+}
 
-    mkdir -p "$ROOT/"{empty,items}
-    touch "$ROOT/items/emptyfile"
-    echo file_in_root > "$ROOT/file_in_root"
-    echo lowercase > "$ROOT/items/lowercase"
-    echo UPPERCASE > "$ROOT/items/UPPERCASE"
-    echo MixedCase > "$ROOT/items/MixedCase"
+cleanup_files=""
+function create_test_files() {
+    touch $* || FATAL "could not create test files"
+    cleanup_files="$cleanup_files $*"
+}
+
+cleanup_dirs=""
+function create_test_dirs() {
+    mkdir -p $* || FATAL "coult not create test directories"
+    cleanup_dirs="$cleanup_dirs $*"
+}
+
+function setup() {
+    create_test_dirs $ROOT/{empty,items} $ROOT $MNT scratch
+    create_test_files $ROOT/file_in_root $ROOT/items/{lowercase,UPPERCASE,MixedCase}
 
     make || exit 1
-    ./icasefs -log_filename=icasefs.log "$MNT" "$ROOT" &
+    ./icasefs -log_filename=icasefs.log $MNT $ROOT &
 
     # Hacky sleep to wait for the filesystem to come up.
-    sleep 1
+    sleep 0.2
 }
 
 function teardown() {
-    fusermount -u "$MNT"
-    rm "$ROOT"/file_in_root \
-        "$ROOT"/items/{emptyfile,lowercase,UPPERCASE,MixedCase}
-    rmdir "$ROOT"/{empty,items} "$ROOT" "$MNT" scratch
+    fusermount -u $MNT
+    rm $cleanup_files
+    rmdir $cleanup_dirs
 }
 
 failure=0
 function FAIL() {
-    echo $* >&2
+    echo "FAIL: $*" >&2
     failure=1
 }
 
 function ASSERT_EXISTS() {
-    [ -e "$1" ] || FAIL "Does not exist: $1"
+    for f in $*; do
+        [ -e "$f" ] || FAIL "Does not exist: $f"
+    done
 }
 
 setup
 
 # Files whose name exists as-is should exist as normal.
-ASSERT_EXISTS "$MNT/"
-ASSERT_EXISTS "$MNT/empty"
-ASSERT_EXISTS "$MNT/items"
-ASSERT_EXISTS "$MNT/items/lowercase"
-ASSERT_EXISTS "$MNT/items/UPPERCASE"
-ASSERT_EXISTS "$MNT/items/MixedCase"
+ASSERT_EXISTS $MNT/{,empty,items}
+ASSERT_EXISTS $MNT/items/{lowercase,UPPERCASE,MixedCase}
 
 # Files whose name case differences should exist
-ASSERT_EXISTS "$MNT/eMpTy"
-ASSERT_EXISTS "$MNT/ItEms"
-ASSERT_EXISTS "$MNT/items/loWERCase"
-ASSERT_EXISTS "$MNT/items/UPpercASE"
-ASSERT_EXISTS "$MNT/items/MiXEDcase"
+ASSERT_EXISTS $MNT/eMpTy
+ASSERT_EXISTS $MNT/ItEms
+ASSERT_EXISTS $MNT/items/{loWERCase,UPpercASE,MiXEDcase}
 
 # Files whose parent directories' case differs should exist.
-ASSERT_EXISTS "$MNT/ItEms"
-ASSERT_EXISTS "$MNT/iTEms/loWERCase"
-ASSERT_EXISTS "$MNT/iTEms/UPpercASE"
-ASSERT_EXISTS "$MNT/iTEms/MiXEDcase"
+ASSERT_EXISTS $MNT/ItEms
+ASSERT_EXISTS $MNT/iTEms/{loWERCase,UPpercASE,MiXEDcase}
 
 teardown
 
